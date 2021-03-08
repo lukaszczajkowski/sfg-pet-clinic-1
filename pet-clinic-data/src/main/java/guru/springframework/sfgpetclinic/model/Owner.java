35242140bd8fa4ca0c46c39cff96dc76d3b15884
Lukasz Czajkowski <lukaszczajkowski.wroc@gmail.com>
2021-03-08 22:58:15 +0100

    Lombok added

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,12 +1,40 @@
 package guru.springframework.sfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
 @Entity
 @Table(name = "owners")
 public class Owner extends Person{
+
+
+    @Builder
+    public Owner(Long id,
+                 String firstName,
+                 String lastName,
+                 Set<Pet> pets,
+                 String address,
+                 String city,
+                 String telephone) {
+        super(id, firstName, lastName);
+        this.pets = pets;
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone;
+    }
+
+    public Owner(Set<Pet> pets, String address, String city, String telephone) {
+        this.pets = pets;
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone;
+    }
 
     @Column(name = "pets")
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
@@ -21,35 +49,4 @@ public class Owner extends Person{
     @Column(name = "telephone")
     private String telephone;
 
-    public Set<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(Set<Pet> pets) {
-        this.pets = pets;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
 }
